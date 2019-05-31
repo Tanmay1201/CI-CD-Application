a2b95a9e9534040dccd18dbe300a7fc546cd6ba9
Tanmay Mathur <36122870+Tanmay1201@users.noreply.github.com>
2019-05-30 22:28:43 -0700

    Update app.py

--- app.py
+++ app.py
@@ -5,7 +5,6 @@ import socket
 
 # Connect to Redis
 redis = Redis(host="redis", db=0, socket_connect_timeout=2, socket_timeout=2)
-print This is tanmay 
 app = Flask(__name__)
 
 @app.route("/")
@@ -15,7 +14,7 @@ def hello():
     except RedisError:
         visits = "<i>cannot connect to Redis, counter disabled</i>"
 
-    html = "<h3>Hello from GitHub {name}!</h3>" \
+    html = "<h3>Hello from Tanmay {name}!</h3>" \
            "<b>Hostname:</b> {hostname}<br/>" \
            "<b>Visits:</b> {visits}"
     return html.format(name=os.getenv("NAME", "Hritik Samaria"), hostname=socket.gethostname(), visits=visits)
